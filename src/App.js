64bc1af85d1e3a9b360f0e270704df0d7e0c789e
Micah <contact_micah@yahoo.com>
2024-06-27 11:31:41 -0700

    4 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { get } from 'aws-amplify/api';
+import { useState } from 'react'
+
+const myAPI = "apijun2724"//"https://c7hgw5za5e.execute-api.us-west-2.amazonaws.com/dev"//"June07AmplifyLambda2"
+const path = "/customers"; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+
+  
+  
+  
+  async function getCustomer(e) {
+    let customerId = e.input
+    const restOperation = get({apiName: myAPI, path: path + "/" + customerId})
+    const { body } = await restOperation.response;
+    const response = await body.json();
+    console.log(response)
+    let newCustomers = [...customers]
+    newCustomers.push(response)
+    setCustomers(newCustomers)
+  }
+  
+  
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Simple React App</h1>
+      <div>
+          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       customers.map((thisCustomer, index) => {
+         return (
+        <div key={thisCustomer.customerId}>
+          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import awsmobile from "./aws-exports";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(awsmobile);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
